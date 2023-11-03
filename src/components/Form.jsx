617933671d2db7df7a38e99905168ae0321f5c6a
Zamua Halera <zamuahalera@mbp-de-zamua.lan>
2023-11-03 10:41:00 +0100

    version with components for form, customInput and sectionTwo

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -41,50 +41,7 @@ const Form = (props) => {
           label="confirmPassword"
           title="Confirm Password"
         />
-        {/* <label htmlFor="username">Name</label>
-        <input
-          onChange={(event) => {
-            props.onChange(event, props.username[1]);
-          }}
-          type="text"
-          id="username"
-          name="username"
-          placeholder="Jean Dupont"
-          value={props.username[0]}
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          onChange={(event) => {
-            props.onChange(event, props.email[1]);
-          }}
-          type="text"
-          id="email"
-          name="email"
-          placeholder="jeandupont@lereacteur.io"
-          value={props.email[0]}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          onChange={(event) => {
-            props.onChange(event, props.password[1]);
-          }}
-          type="password"
-          id="password"
-          name="password"
-          placeholder="lErEaCtEuR2020"
-          value={props.password[0]}
-        />
-        <label htmlFor="confirmPassword">Confirm your password</label>
-        <input
-          onChange={(event) => {
-            props.onChange(event, props.confirmPassword[1]);
-          }}
-          type="password"
-          id="confirmPassword"
-          name="password"
-          placeholder="lErEaCtEuR2020"
-          value={props.confirmPassword[0]}
-        /> */}
+
         <input type="submit" value="Register" />
       </form>
     </>
